ad25c9d042f1deabe3e50f48ea2d6c2246af7a27
Zuxevil1 <144577265+Zuxevil1@users.noreply.github.com>
2025-07-05 16:17:42 +0700

    Delete Backend Rest API directory

--- Backend Rest API/backend/src/main/java/com/example/backend/repository/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.History;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoryRepository extends JpaRepository<History, Integer> {
-}
